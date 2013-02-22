a5f886671265201582f838f6342b02eea22f417e
Nathan <nathan.rosandich@lasra.co.nz>
2013-02-22 13:09:23 +1300

    Update Unsteady state heat transfer/php/table_display.php

--- Unsteady state heat transfer/php/table_display.php
+++ Unsteady state heat transfer/php/table_display.php
@@ -1,3 +1,9 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>Cooling of Hides and Skins</title>
+</head>
+<body>
 <?php session_start();
     $array = unserialize(urldecode($_SESSION['serialized_data']));
     echo "<table border=\"1\">";
@@ -16,3 +22,5 @@
     }
     echo "</table>";
 ?>
+</body>
+</html>
